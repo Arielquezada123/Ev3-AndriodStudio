b00561825df8c8fee6544d979615e01acef2d3b9
Ariel <ariel.quezada06@inacapmail.cl>
2023-12-12 18:04:27 -0300

    Fusion

--- app/src/main/java/com/example/prueba_2/SettingsFragment.kt
+++ app/src/main/java/com/example/prueba_2/SettingsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.prueba_2
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.prueba_2.databinding.FragmentSettingsBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -27,37 +29,36 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         auth = FirebaseAuth.getInstance()
+
         binding.btnLogout.setOnClickListener {
-            auth.signOut()
-            val intent = Intent(activity, MainActivity::class.java)
+            // Configurar el diálogo de cierre de sesión
+            AlertDialog.Builder(requireContext())
+                .setTitle("Cerrar Sesión")
+                .setMessage("¿Estás seguro de que quieres cerrar sesión?")
+                .setPositiveButton("Sí") { _, _ ->
+                    // Respond to positive button press
+                    auth.signOut()
+                    val intent = Intent(requireActivity(), MainActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    // Respond to negative button press
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        binding.btnContra.setOnClickListener {
+            val intent = Intent(requireActivity(), CambiarContraActivity2::class.java)
             startActivity(intent)
-            activity?.finish()
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
